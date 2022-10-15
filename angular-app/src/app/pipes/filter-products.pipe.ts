846b9c42937c74e448ffba49df81a9960d7d6da6
Igor Burenkov <49118930+BrNkV@users.noreply.github.com>
2022-10-15 17:54:04 +0300

    feat: add filter pipe

---
+++ angular-app/src/app/pipes/filter-products.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { IProduct } from '../models/product';
+
+@Pipe({
+  // название котор будем использовать в шаблоне
+  name: 'filterProducts'
+})
+export class FilterProductsPipe implements PipeTransform {
+
+  // принимает значение продукты и строку поиска, на выходе продукт
+  transform(products: IProduct[], search: string): IProduct[] {
+    // логика для отображения фильтрации
+    return products.filter(p => p.title.toLowerCase().includes(search.toLowerCase()))
+  }
+
+}
